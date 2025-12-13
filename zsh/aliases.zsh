1083f1babd1f89d0636c7676d3caa90be84a4db7
Yernur Kalikhan <yernur.kalikhan@gmail.com>
2025-12-13 17:38:45 +0500

    feat: colored ls aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,8 +6,8 @@ alias ga='git add'
 alias ..='cd ..'
 alias ...='cd ../..'
 
-alias l='ls -lAh'
-alias ls='ls -a'
-alias ll='ls -al'
+alias l='ls -lAh --color'
+alias ls='ls -a --color'
+alias ll='ls -al --color'
 
 alias grep='grep --color=auto'
